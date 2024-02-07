535780153331d0c381990dbbf60705ea54ad5bc0
Benjamin <benjamin@benjamin>
2024-02-07 15:15:55 +0100

    Classe terminée et exception de type IllegalArgumentException

--- src/main/java/com/polytech/CustomTime.java
+++ src/main/java/com/polytech/CustomTime.java
@@ -1,4 +1,59 @@
 package com.polytech;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class CustomTime {
+    private byte heures;
+    private byte minutes;
+
+    public CustomTime(int heures, int minutes){
+        this.setHeures(heures);
+        this.setMinutes(minutes);
+    }
+
+    public CustomTime(){
+        LocalDateTime temps = LocalDateTime.now(); // A formater
+        DateTimeFormatter formateurDate = DateTimeFormatter.ofPattern("HH:mm");
+        String formatDate = temps.format(formateurDate);
+        String[] parts = formatDate.split(":");
+        this.setHeures(Integer.parseInt(parts[0]));
+        this.setMinutes(Integer.parseInt(parts[1]));
+    }
+
+    public String toString(){
+        return "Il est : "+heures+"heure(s) et "+minutes+"minute(s)";
+    }
+
+    public int getHeures(){
+        return heures;
+    }
+
+    public int getMinutes(){
+        return minutes;
+    }
+
+    public void setHeures(int hh){
+        // Pas necessaire de propager le throw avec throws car c'est une RuntimeException
+        if(hh < 0 || hh > 23){
+            throw new IllegalArgumentException("Heure invalide");
+        } else {
+            this.heures = (byte)hh;
+        }
+    }
+
+    public void setMinutes(int mm){
+        if(mm < 0 || mm > 59){
+            throw new IllegalArgumentException("Heure invalide");
+        } else {
+            this.heures = (byte)mm;
+        }
+    }
+
+    public void setHeuresMinutes(int hh, int mm){
+        this.setHeures(hh);
+        this.setMinutes(mm);
+    }
+
+
 }
--- src/main/java/com/polytech/Main.java
+++ src/main/java/com/polytech/Main.java
@@ -2,6 +2,18 @@ package com.polytech;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CustomTime time1 = new CustomTime();
+        System.out.println(time1);
+
+        CustomTime time2 = new CustomTime(11,11);
+        System.out.println(time2);
+
+        try {
+            CustomTime time3 = new CustomTime(-34, 72);
+            System.out.println(time3);
+        } catch (IllegalArgumentException e) {
+            // Permet de ne pas arrêter le programme si une heure est invalide
+            System.out.println(e.getMessage());
+        }
     }
 }
